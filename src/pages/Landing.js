6b37fa69bffca47a6f794010b6bb44d549d1d581
Yevgeniy343 <rt4387@yahoo.com>
2023-04-25 15:03:21 +0300

    verv

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,4 +1,5 @@
-import Wrapper from "../wrappers/landing-wr";
+import styled from "styled-components";
+
 import { Link } from "react-router-dom";
 
 const Landing = () => {
@@ -21,5 +22,28 @@ const Landing = () => {
     </Wrapper>
   );
 };
+const Wrapper = styled.main`
+  margin: 2rem;
+  font-family: "Yeon Sung", cursive;
+
+  .page-content {
+    display: grid;
+    grid-template-columns: 1fr;
+    color: var(--theme-ui-colors-green70);
+    h1 {
+      font-size: 3rem;
+    }
+    p {
+      font-size: 1.3rem;
+    }
+  }
+  @media (min-width: 992px) {
+    .page-content {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 3rem;
+    }
+  }
+`;
 
 export default Landing;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,4 @@
-import Wrapper from "../wrappers/register-wr";
+import styled from "styled-components";
 import FormRow from "../components/FormRow";
 import { Link } from "react-router-dom";
 import { HiArrowNarrowLeft } from "react-icons/hi";
@@ -109,5 +109,58 @@ const Register = () => {
     </Wrapper>
   );
 };
+const Wrapper = styled.main`
+  font-family: "Yeon Sung", cursive;
 
+  svg {
+    color: var(--theme-ui-colors-green70);
+    width: 2rem;
+    border: 1px solid var(--theme-ui-colors-green70);
+    font-size: 1.3rem;
+  }
+  svg:hover {
+    background-color: var(--theme-ui-colors-green20);
+    border-radius: 40px;
+  }
+  .input-content {
+    text-align: center;
+  }
+  h1 {
+    text-align: center;
+  }
+  button {
+    font-family: "Yeon Sung", cursive;
+    font-size: 1.3rem;
+  }
+  .actions {
+    margin: 1rem;
+    text-align: center;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--theme-ui-colors-green50);
+    cursor: pointer;
+    font-size: 1rem;
+  }
+  .input-decoration {
+    border: none;
+    border-bottom: 7px solid var(--theme-ui-colors-green50);
+    font-family: "Yeon Sung", cursive;
+    font-size: 1.3rem;
+    caret-color: var(--theme-ui-colors-yellow50);
+    :focus-visible {
+      outline: none;
+      background-color: var(--theme-ui-colors-green20);
+      ::placeholder {
+        font-size: 1rem;
+      }
+    }
+    ::placeholder {
+      color: gray;
+    }
+  }
+  @media (min-width: 992px) {
+  }
+`;
 export default Register;
