1112535f50593d18f2f9f588f45f0128971b2278
Yevgeniy343 <rt4387@yahoo.com>
2023-07-31 20:08:26 +0300

    submenu

--- src/components/adminNavigations/Submenu.js
+++ src/components/adminNavigations/Submenu.js
@@ -12,7 +12,7 @@ const Submenu = () => {
 
   return (
     <Wrapper>
-      <div className="submenu">
+      <div className={currentPage ? "submenu show-submenu" : "submenu"}>
         <h5>{currentPage?.page}</h5>
         <div
           className="submenu-links"
@@ -32,11 +32,47 @@ const Submenu = () => {
 };
 
 const Wrapper = styled.div`
+  .submenu {
+    display: none;
+  }
   @media (min-width: 576px) {
   }
   @media (min-width: 768px) {
   }
   @media (min-width: 992px) {
+    .submenu {
+      display: block;
+      position: fixed;
+      top: 6rem;
+      width: 90vw;
+      background-color: white;
+      left: 50%;
+      padding: 2rem;
+      transform: rotateX(-90deg) translateX(-50%);
+      transform-origin: top;
+      perspective: 1000px;
+      visibility: hidden;
+      border-radius: 5px;
+      opacity: 0;
+      transition: transform 0.3s ease-in-out, opacity 0.2s ease-in-out;
+      h5 {
+        margin-bottom: 1rem;
+        color: var(--blue-0);
+      }
+    }
+    .show-submenu {
+      visibility: visible;
+      opacity: 1;
+      transform: rotateX(0deg) translate(-50%);
+    }
+    .submenu-links {
+      display: grid;
+      row-gap: 0.5rem;
+      p {
+        display: block;
+        color: var(--blue-0);
+      }
+    }
   }
   @media (min-width: 1200px) {
   }
